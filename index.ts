4a163419d37c6c22b9f675a1d663f891cad030ed
Oliverio <oliverio.junior2@gmail.com>
2021-12-05 17:48:54 -0300

    Corrigir função

--- index.ts
+++ index.ts
@@ -3,9 +3,9 @@ let number_ = document.querySelector("#number-2")
 let button = document.querySelector(".somar")
 let resultado = document.querySelector(".resultado")
  
-button.addEventListener("click",(a: number,b: number)=>{
+/* button.addEventListener("click",(a: number,b: number)=>{
     a = number.value
     b = number_.value
     
     resultado.innerText = a*b
-})
+}) */
