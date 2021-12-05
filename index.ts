2a56c83c2be3a7786ec325ec2193efb812499d65
Oliverio <oliverio.junior2@gmail.com>
2021-12-05 17:43:26 -0300

    Estudos typeScript

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log('TypeScript')
+let number = document.querySelector("#number-1")
+let number_ = document.querySelector("#number-2")
+let button = document.querySelector(".somar")
+let resultado = document.querySelector(".resultado")
+ 
+button.addEventListener("click",(a: number,b: number)=>{
+    a = number.value
+    b = number_.value
+    
+    resultado.innerText = a*b
+})
